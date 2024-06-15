8998e9a7bdc96a55dd418c57ade258eac26d5335
abukuev <abukuev@list.ru>
2024-06-15 16:49:44 +0300

    ???

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -27,7 +27,7 @@ SESSION_COOKIE_SECURE=True
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com/']
+ALLOWED_HOSTS = ['*']
 
 CSRF_COOKIE_SECURE = True
 # Application definition
